084698b7322003fa757406aeb330b0bf0b1ae84d
Theresececilia <therese.johansson@edu.cmeducations.se>
2023-02-10 09:42:47 +0100

    add correct information and restructure files

--- index.js
+++ index.js
@@ -1,50 +1,61 @@
 import chalk from 'chalk';
 import fs from 'fs/promises'
-import {formatDistanceToNow, differenceInDays, parse, set} from 'date-fns'
+import {formatDistanceToNow, differenceInDays, format, isAfter, isBefore, parseISO} from 'date-fns'
 import {Command} from 'commander';
 import getGitVersion from './src/getGitVersion.js';
 
 const gitVersion = await getGitVersion()
-console.log(`git version: ${gitVersion}`);
-
 const first = 'Therese'
 const last = 'Johansson'
 const name = `${chalk.bgBlue.magenta(first)} ${chalk.bgMagenta.blue(last)}`
 const info = process.env.npm_config_user_agent
-console.log('name', name)
-console.log(`npm & node: ${info}`)
-
 const startOfCourse = new Date(2023, 0, 31);
 const courseStartToNow = formatDistanceToNow(startOfCourse);
-console.log('Since we have started javascript frameworks its been', courseStartToNow)
-
 const argumentParser = new Command();
 argumentParser.option('--date')
 argumentParser.parse();
 
 const dateStringSentAsArgument = argumentParser.args[0]
-const dateSentAsArgument = parse(dateStringSentAsArgument, 'yyyy-MM-dd', new Date())
-const currentDate = set(new Date(), {hours: 0, minutes: 0, seconds: 0})
+const dateSentAsArgument = parseISO(dateStringSentAsArgument, 'yyyy-MM-dd', new Date())
+const currentDate = new Date()
 const difference = compareDates(dateSentAsArgument);
+const formattedDate = format(currentDate, 'MM/dd/yyyy hh:mm a');
+const isBeforeToday = isBefore(dateSentAsArgument, currentDate)
+const isAfterToday = isAfter(dateSentAsArgument, currentDate)
+let beforeOrAfter = '';
 
-console.log(`Date sent in as Argument ${dateSentAsArgument}`)
-console.log('Today is :',(currentDate))
+
+
+if (isBeforeToday === true) {
+ beforeOrAfter = 'Your date is before today.';
+} else if (isAfterToday === true) {
+  beforeOrAfter = 'Your date is after today.';
+} else {
+  beforeOrAfter = 'Your date is today.';
+}
 
 function compareDates(date) {
     const difference = differenceInDays(date, currentDate);
     return difference;
   }
 
-  console.log(`Number of days between todays date ${currentDate} and your argument date ${dateSentAsArgument} is: ${difference}`);
+console.log(`git version: ${gitVersion}`);
+console.log('name', name)
+console.log(`npm & node: ${info}`)
+console.log('Since we have started javascript frameworks its been', courseStartToNow)
+console.log('Today is :',(formattedDate))
+console.log(`Date sent in as Argument ${dateSentAsArgument}`)
+console.log(`Number of days between todays date ${formattedDate} and your argument date ${dateSentAsArgument} is: ${difference}`);
 
 const fileContent = `
 name: ${first} ${last}
 npm & node: ${info}
 git version: ${gitVersion}
-Today's date is ${currentDate}
-Date sent in as Argument ${dateSentAsArgument}
+Today's date is ${formattedDate}
+Date you sent in as an argument ${dateSentAsArgument}
+${beforeOrAfter}
+Number of days between todays date and your date is: ${difference}
 Since we have started javascript frameworks its been ${courseStartToNow}
-Number of days between todays date ${currentDate} and your argument date ${dateSentAsArgument} is: ${difference}
 `;
 
 const htmlContent = `
@@ -66,10 +77,11 @@ const htmlContent = `
       <li>Name: ${first} ${last}</li>
       <li>Npm & node: ${info}</li>
       <li>Git version: ${gitVersion}</li>
-      <li>Today's date is ${currentDate}</li>
-      <li>Date sent in as Argument ${dateSentAsArgument}</li>
+      <li>Today's date is ${formattedDate}</li>
+      <li>Date you sent in as an argument ${dateSentAsArgument}</li>
+      <li>${beforeOrAfter}</li>
+      <li>Number of days between todays date and your date is: ${difference}</li>
       <li>Since we have started javascript frameworks its been ${courseStartToNow}</li>
-      <li>Number of days between todays date ${currentDate} and your argument date ${dateSentAsArgument} is: ${difference}</li>
     </ul>
     </div>
 </body>
--- index.md
+++ index.md
@@ -2,7 +2,8 @@
 name: Therese Johansson
 npm & node: npm/8.19.3 node/v18.13.0 win32 x64 workspaces/false
 git version: git version 2.38.1.windows.1
-Today's date is Thu Feb 09 2023 00:00:00 GMT+0100 (Central European Standard Time)
-Date sent in as Argument Sat Jun 10 2023 00:00:00 GMT+0200 (Central European Summer Time)
+Today's date is 02/10/2023 09:41 AM
+Date you sent in as an argument Mon Apr 10 2023 00:00:00 GMT+0200 (Central European Summer Time)
+Your date is after today.
+Number of days between todays date and your date is: 58
 Since we have started javascript frameworks its been 10 days
-Number of days between todays date Thu Feb 09 2023 00:00:00 GMT+0100 (Central European Standard Time) and your argument date Sat Jun 10 2023 00:00:00 GMT+0200 (Central European Summer Time) is: 120
